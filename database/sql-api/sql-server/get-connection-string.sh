fffb4e9ce0229217c0982519d54c2ce8cc6e0f3c
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-02-24 21:46:40 -0800

    connection string

--- database/sql-api/sql-server/get-connection-string.sh
+++ database/sql-api/sql-server/get-connection-string.sh
@@ -1,7 +1,6 @@
 az sql db show-connection-string \
 --subscription YOUR-SUBSCRIPTION-ID-OR-NAME \
---resource-group YOUR-RESOURCE-GROUP \
 --server YOUR-RESOURCE-NAME \
---name YOUR-DATABASE-NAME \
+--name YOUR-DATABASE-NAME
+--client odbc \
 --auth-type SqlPassword 
-
